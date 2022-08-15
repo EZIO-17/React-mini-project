162334f9fb2d488e62f2c1a87261782d0620b274
EZIO-17 <samatreya@gmail.com>
2022-08-15 23:59:36 +0530

    Added an error modal

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,14 +9,23 @@ const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredEmail, setEnteredEmail] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState
 
     const addUserHandler = (event) => {
         event.preventDefault();
         if (enteredUsername.trim().length === 0 || enteredEmail.trim().length === 0 || enteredAge.trim().length === 0){
+            setError({
+                title: "An Error Occured!",
+                message: "Something went wrong!",
+            });
             return;
         }
 
         if (enteredAge < 1){
+            setError({
+                title: "An Error Occured!",
+                message: "Something went wrong!",
+            });
             return;
         }
 
@@ -38,10 +47,14 @@ const AddUser = (props) => {
     const ageChangeHandler = (event) => {
         setEnteredAge(event.target.value);
     }
+
+    const errorHandler = () => {
+        setError(null);
+    }
     
     return (
         <div>
-            <ErrorModal title = "An Error Occured!"  message = "Something went wrong!" />
+            {error && <ErrorModal title = {error.title}  message = {error.message} onConfirm = {errorHandler} />}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor='username'>Username</label>
